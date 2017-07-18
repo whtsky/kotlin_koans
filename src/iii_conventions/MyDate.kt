96019a693bd3508c2584a285d585ff846c40a824
Wu Haotian <whtsky@gmail.com>
2017-07-18 09:25:44 +0800

    Finish conventions

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +16,29 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(n: Int) = RepeatedTimeInterval(this, n)
+
+class DataIterator(var current: MyDate, val end: MyDate): Iterator<MyDate> {
+    override operator fun next(): MyDate {
+        val rv = current
+        current = current.nextDay()
+        return rv
+    }
+
+    override operator fun hasNext(): Boolean = current <= end
+}
+
+class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate> {
+    operator fun contains(d: MyDate): Boolean {
+        return start <= d && d <= endInclusive
+    }
+
+    override operator fun iterator(): Iterator<MyDate> {
+        return DataIterator(start, endInclusive)
+    }
+}
+
+operator fun MyDate.plus(t: TimeInterval): MyDate = addTimeIntervals(t, 1)
+operator fun MyDate.plus(t: RepeatedTimeInterval): MyDate = addTimeIntervals(t.timeInterval, t.number)
